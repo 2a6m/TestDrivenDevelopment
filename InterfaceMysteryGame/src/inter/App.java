4e6eecf3ab90fc13ff9e34ca6ab5808528a04d1d
Julien Beard <beardjulien@gmail.com>
2018-11-01 19:11:07 +0100

    Create the first basic java interface

---
+++ InterfaceMysteryGame/src/inter/App.java
@@ -0,0 +1,30 @@
+package inter;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JPanel InterfaceMain;
+    private JLabel Title;
+    private JLabel Intro;
+    private JComboBox Difficulty;
+    private JButton PlayButton;
+
+    public App() {
+        PlayButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hello world");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MysteryGame");
+        frame.setContentPane(new App().InterfaceMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
