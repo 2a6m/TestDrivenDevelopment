d58c31d8a4be52c5fed298bae1726f69ef4a70ee
Julien Beard <beardjulien@gmail.com>
2018-11-02 11:39:25 +0100

    Create the second java interface

--- InterfaceMysteryGame/src/inter/App.java
+++ InterfaceMysteryGame/src/inter/App.java
@@ -15,7 +15,12 @@ public class App {
         PlayButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Hello world");
+                //JOptionPane.showMessageDialog(null, "Hello world");
+                JFrame frame = new JFrame("MysteryGame");
+                frame.setContentPane(new GameChoice().getGameChoice());
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
             }
         });
     }
---
+++ InterfaceMysteryGame/src/inter/GameChoice.java
@@ -0,0 +1,15 @@
+package inter;
+
+import javax.swing.*;
+
+public class GameChoice {
+    private JPanel gameChoice;
+    private JLabel title;
+    private JLabel intro;
+    private JComboBox comboBox1;
+    private JButton button1;
+
+    public JPanel getGameChoice() {
+        return gameChoice;
+    }
+}
