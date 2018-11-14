e687152a0800be5471bde3f6257e6a1f39505924
WhiteShadow13 <ludo.m@live.fr>
2018-11-14 16:59:08 +0100

    Update RemoveLetter
    the function remove 3 letters

--- libraries/src/Main.java
+++ libraries/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String [] args) {
 
         SentenceMystery typeMystery = new SentenceMystery();
-        Difficulty difficulty = new Difficulty("advanced");
+        Difficulty difficulty = new Difficulty("easy");
 
         Game game = new Game(difficulty, typeMystery);
         game.SetTypeMystery(typeMystery);
