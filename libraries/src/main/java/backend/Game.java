62dd7f5fc072138d7241bf0c031679e29997a069
Houdaaaa <Houda-11.03@hotmail.com>
2018-11-27 19:58:10 +0100

    Documentation for Game and Difficulty Classes
    Omg last commit of our project, good luck for the successors

--- libraries/src/main/java/backend/Game.java
+++ libraries/src/main/java/backend/Game.java
@@ -23,27 +23,66 @@ public class Game {
 
     /* To play an other gameMystery
      *
-     * @param
      * @return void
      */
     public void NextMystery() { typeMystery.NextMystery(this); }
 
+    /* Modifies the letters to display through the interface
+     *
+     * @param  letterDisplay   the new letters to display
+     * @return void
+     */
     public void SetLetterDisplay(ArrayList<Character> letterDisplay) { this.letterDisplay = letterDisplay; }
 
+    /* Get the letters to display through the interface
+     *
+     * @return an ArrayList with the letters to display
+     */
     public ArrayList<Character> GetLetterDisplay() { return this.letterDisplay; }
 
+    /* Modifies the letters to display through the interface
+     *
+     * @param  letterDisplay   the new letters to display
+     * @return void
+     */
     public void SetTypeMystery(TypeMystery typeMystery) { this.typeMystery = typeMystery; }
 
+    /* Modifies the Difficulty of the game
+     *
+     * @param  difficulty      the new difficulty that the user has chosen
+     * @return void
+     */
     public void SetDifficulty(Difficulty difficulty) { this.difficulty = difficulty; }
 
+    /* Gives the Difficulty of the game
+     *
+     * @return an object Difficulty representing the difficulty of the current game
+     */
     public Difficulty GetDifficulty() { return this.difficulty; }
 
+    /* Changes the mystery to display through the interface
+     *
+     * @param  mystery      the new mystery to display (a map where the key is the response of the mystery and the value is the mystery)
+     * @return void
+     */
     public void SetMystery(Pair<String, List<String>> mystery) { this.mystery = mystery; }
 
+    /* Gives the current mystery of the game
+     *
+     * @return void
+     */
     public Pair<String, List<String>> GetMystery() { return mystery; }
 
+    /* Gives the available bonuses
+     *
+     * @return          a ArrayList of TypeBonus representing the available bonuses
+     */
     public ArrayList<TypeBonus> GetBonusList() { return bonusList; }
 
+    /* Gives the current player who is playing
+     *
+     * @return a Player object representing the current player
+     */
     public Player GetPlayer() { return player; }
 
     /* Connect and initialize a player
